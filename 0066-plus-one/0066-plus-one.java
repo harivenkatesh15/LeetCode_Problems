9725b6a8a143a4eb784855f9890dc7d8aa1d1c6b
Hari <134863582+harivenkatesh15@users.noreply.github.com>
2026-01-01 12:34:18 +0530

    Time: 0 ms (100%), Space: 43.2 MB (77.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,31 +1,24 @@
 class Solution {
     public int[] plusOne(int[] arr) {
-        int n = arr.length-1;
-        if(arr[n]!=9){
-            arr[n] = arr[n]+1;
-            return arr;
-        }
-
-        while(n>=0 && arr[n]==9){
-            arr[n] = 0;
-            n--;
-        }
-        int prev = 0;
-        if(n>=0){
-            prev = arr[n];
-        }
-        int nn = arr.length;
-        if(n<0){
-            n = n+1;
-            nn = arr.length+1;
-        }
-        int[] nums = new int[nn];
-        for(int i = 0;i<n;i++){
-            nums[i] = arr[i];
-        }
-
-        nums[n] = prev+1;
-        return nums;
-        
+         
+          int n = arr.length;
+           if(arr[n-1]!=9){
+                arr[n-1] = arr[n-1]+1;
+                return arr;
+           }
+           int ind = 0;
+           for(int i = arr.length-1;i>=0;i--){
+                if(arr[i]!=9){
+                    arr[i] = arr[i]+1;
+                    return arr;
+                }
+                else{
+                    arr[i] = 0;
+                }
+            }
+           
+            int[] ar = new int[n+1];
+            ar[0] = 1;
+            return ar;
     }
 }
