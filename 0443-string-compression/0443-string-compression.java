8c82a9ecda0b3f5b0ba40abfb53825b3b92d7a89
Hari <134863582+harivenkatesh15@users.noreply.github.com>
2026-02-12 09:45:43 +0530

    Time: 1 ms (99.69%), Space: 45.6 MB (30.21%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,45 +1,24 @@
 class Solution {
     public int compress(char[] chars) {
-                if(chars.length==1){
-                    return 1;
+        int n = chars.length;
+        int write = 0;   
+        int i = 0;       
+        while (i < n) {
+            char currentChar = chars[i];
+            int count = 0;
+            while (i < n && chars[i] == currentChar) {
+                i++;
+                count++;
+            }
+            chars[write++] = currentChar;
+            if (count > 1) {
+                String countStr = String.valueOf(count);
+                for (char c : countStr.toCharArray()) {
+                    chars[write++] = c;
                 }
-                
-                char prev = chars[0];
-                int c = 1;
-                int cc = 1;
-                ArrayList<Character> ans = new ArrayList<>();
-                int i = 1;
-                for(i = 1;i<chars.length;i++){
-                    if(prev!=chars[i]){
-                        ans.add(prev);
-                        String s = ""+c;
-                        prev = chars[i];
-
-                        if(c==1){
-                            c = 1;
-                            continue;
-                        }
-                        for(int k = 0;k<s.length();k++) {
-                            ans.add(s.charAt(k));
-                        }
-                        c = 1;
-
-                    }
-                    else{
-                        c++;
-                    }
-                }
-                ans.add(chars[--i]);
-        String s = ""+c;
-        if(c!=1) {
-            for (int k = 0; k < s.length(); k++) {
-                ans.add(s.charAt(k));
             }
         }
-        int k = 0;
-        for(k = 0;k<ans.size();k++){
-            chars[k] = ans.get(k);
-        }
-        return k;
+        
+        return write;
     }
-    }
+}
