377018e9170692ef81933c756cdf501d1c01de05
Hari <134863582+harivenkatesh15@users.noreply.github.com>
2026-02-13 12:52:56 +0530

    Time: 1 ms (98.54%), Space: 47.8 MB (74.64%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] arr, int n) {
+        int nn = arr.length;
+        if(arr[0]==0 && nn==1){
+            return true;
+        }
+        if(nn>=2 && arr[0]==0 && arr[1]!=1){
+            arr[0] = 1;
+            n--;
+        }
+        if(n==0) return true;
+        for(int i = 1;i<arr.length-1;i++){
+            if(arr[i]==0 && arr[i-1]!=1 && arr[i+1]!=1 && n!=0){
+                arr[i] =1;
+                n--;
+            }
+            if(n==0){
+                break;
+            }
+        }
+        if(nn>=2 &&arr[nn-1]==0 && arr[nn-2]!=1 && n!=0){
+            n--;
+        }
+      
+        if(n<=0) return true;
+        return false;
+    }
+}
